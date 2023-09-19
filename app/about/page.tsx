0c6665c2d0ce4ccab50c07332cfc84291a61fd1c
mridul <thapliyamridul19@gmail.com>
2023-09-19 17:57:47 +0530

    initial commit

---
+++ app/about/page.tsx
@@ -0,0 +1,11 @@
+import Link from "next/link"
+
+export default function Home() {
+    return (
+      <main>
+        <Link href={"/"}>Navigate to home page</Link>
+        <h1>Welcome to the about page</h1>
+      </main>
+    )
+  }
+  
